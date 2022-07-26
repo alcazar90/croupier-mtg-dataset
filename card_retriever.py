248d3d5fcd2b6780d07bb636b66cd38133ae87be
Cristóbal Alcázar <alcazar.cristobal90@gmail.com>
2022-07-25 22:36:25 -0400

    Refactor the whole code using utils functions and exceptions from the gatherer_croupier module

--- card_retriever.py
+++ card_retriever.py
@@ -1,83 +1,139 @@
 """
-    card_retriever.py
-    Script to retrieve information and download card image from card's url.
+    File: card_retriever.py
+
+    The task of this file is to download card features (e.g. text, mana cost, power and toughness, and so on) and the
+    card images from The Gatherer. There are configurations that can handle in gatherer_croupier/config.py such as
+    the path directories and the name of the files.
+
+    Please see the usage section on the README.
 """
 import os
+import ssl
+import csv
+import argparse
+import pandas as pd
 import logging
 import coloredlogs
-import ssl
-import pandas as pd
 from urllib.request import urlretrieve
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
 
-# Define path constants (there could be a file with all the path constants)
-# TODO: Move all constants into a module
-CARD_DIRECTORY_PATH = './data/card_database.csv'
-CARD_IMAGE_PATH = './data/img'
-FILTER = 'Goblin'
+from gatherer_croupier.driver import initialize_driver
+from gatherer_croupier.exceptions import NoCardImageFoundException, NoFeatureAvailableException, \
+    ManaCostSymbolException, NoCardTextAvailableException
+from gatherer_croupier.utils import get_card_text, get_xpath_text, get_mana_cost, get_img_information
+from gatherer_croupier.config import XpathCardFeatures, CARD_DIRECTORY_PATH, CARD_IMAGE_PATH, \
+    CARD_INFORMATION_PATH
 
-# Create logger
+# Initialize logger
 logger = logging.getLogger(__name__)
 coloredlogs.install(level='INFO')
 
-# Apparently this is not the BEST way to do this...instead update certificates
-# https://stackoverflow.com/questions/43204012/how-to-disable-ssl-verification-for-urlretrieve
+# Avoid certificate for download image
 ssl._create_default_https_context = ssl._create_unverified_context
 
-# Read csv with card's url
-cards = pd.read_csv(CARD_DIRECTORY_PATH)
-card_index = cards.loc[cards.subtype == FILTER, 'id']\
-    .sample(5, random_state=42)\
-    .index
-cards = cards.iloc[card_index]
-logger.info(cards)
+# Initialize the parser
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group()
+parser.add_argument("creature_type", help="Enter the creature type")
+group.add_argument("-i", "--card_id", help="Option to download a specific card using its id", type=int)
+group.add_argument("-n", "--number", help="Specify the number of cards to download (randomly choice from dir)",
+                   type=int)
+args = parser.parse_args()
 
+# TODO: check if the file card_database.csv exists in CARD_DIRECTORY_PATH exists. Otherwise, stop the program and
+# return a message that a card_database is required first...
 
-# Check if the data/subtype exists
-logger.info(f'Checking if directory {CARD_IMAGE_PATH}/{FILTER.lower()} exists')
-check_path = os.path.isdir(f'{CARD_IMAGE_PATH}/{FILTER.lower()}"')
-if ~check_path:
-    os.makedirs(f'{CARD_IMAGE_PATH}/{FILTER.lower()}')
-    logger.info(f'The {CARD_IMAGE_PATH}/{FILTER.lower()} directory will be create...')
-else:
-    logger.info('Directory exists')
-CARD_IMAGE_PATH += f'/{FILTER.lower()}'
-
-# Settings for init url-connection and iterate...
-NUM_CARDS = cards.shape[0]
-options = Options()
-options.add_argument("--headless")
 
 if __name__ == '__main__':
+
+    # Open card_directory to get card's url
+    cards = pd.read_csv(CARD_DIRECTORY_PATH)
+    cards = cards.loc[cards.subtype == args.creature_type, :]
+    # TODO: check creature if the creature type is available in the card_database.csv. Raise an exception if not
+
+    if args.number:
+        # If the option -n was specified in the command line
+        cards = cards.sample(args.number, random_state=42)
+    elif args.card_id:
+        cards = cards.loc[cards.id == args.card_id, :]
+
+    NUM_CARDS = cards.shape[0]
+    logging.info(f"[START] There are {NUM_CARDS} {args.creature_type}'s cards to download its information...")
+
+    # Check if the data/subtype exists, following config.py paths
+    logger.info(f'[ACTION] Checking if directory {CARD_IMAGE_PATH}/{args.creature_type.lower()} exists')
+    check_path = os.path.isdir(f"{CARD_IMAGE_PATH}/{args.creature_type.lower()}")
+    if check_path:
+        logger.info('[STATUS] Directory exists')
+    else:
+        logger.info(f"[STATUS] The {CARD_IMAGE_PATH}/{args.creature_type.lower()} directory will be create...")
+        os.makedirs(f"{CARD_IMAGE_PATH}/{args.creature_type.lower()}")
+    CARD_IMAGE_PATH += f"/{args.creature_type.lower()}"
+
     for i in range(NUM_CARDS):
+
         # Establish connection given the card information (from card_database.csv)
-        #---------------------------------------------------------------------------------------------------------------
+        # ---------------------------------------------------------------------------------------------------------------
         # TODO: check w.r.t something (log or file) if the information and image for the current_id it was downloaded
+
         current_id = cards.iloc[i, 0]
-        logger.info('Establish connection...')
-        driver = webdriver.Firefox(options=options)
+        logger.info(f'[PROGRESS] Iteration {i + 1}/{NUM_CARDS}, establish connection for card id {current_id}')
+        driver = initialize_driver(headless=True, install=False)
         img_url = cards.iloc[i, 3]
         driver.get(img_url)
-        logger.info('Connection successful')
-        #text_xpath = "/html/body/form/div[5]/div/div[1]/div[2]/div/div[2]/table/tbody/tr/td[1]/table/tbody/tr/td[2]/div[2]/div[5]/div[2]/div[1]"
+        logger.info('[STATUS] Connection successful')
 
-        # Extract img url given the XPATH and then extracting the 'src' attribute
-        #---------------------------------------------------------------------------------------------------------------
-        # TODO: exception to handle pages with two images
-        # example: https://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=472967
-        img_xpath = '//*[@id="ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cardImage"]'
-        img = driver.find_element(By.XPATH, img_xpath)
-        src = img.get_attribute('src')
-        logger.info(src)
-        logger.info(f'Img source retrieved: {img}')
+        # Extract the image source and extension given the card url and xpath. Download & Save in a folder the image
+        # ---------------------------------------------------------------------------------------------------------------
 
-        # Download the image and save in a folder
-        #---------------------------------------------------------------------------------------------------------------
-        logger.info('Downloading the image and storing in the folder...')
-        urlretrieve(src, f'{CARD_IMAGE_PATH}/{current_id}_{FILTER.lower()}.jpeg')
+        try:
+            img_src, img_ext = get_img_information(driver, XpathCardFeatures.IMAGE.value)
+            logger.info(f'[STATUS] Image source (with {img_ext} extension) retrieved: {img_src}')
+        except NoCardImageFoundException:
+            logger.info(f'[Exception] Skip card {current_id} because doble page format')
+            driver.quit()
+            continue
 
-        # Close connection
-        #---------------------------------------------------------------------------------------------------------------
-        driver.close()
+        logger.info(f'[WRITE] Downloading the image of card {current_id} and storing in the folder '
+                    f'{CARD_IMAGE_PATH}/{current_id}_{args.creature_type.lower()}')
+
+        urlretrieve(img_src, f'{CARD_IMAGE_PATH}/{current_id}_{args.creature_type.lower()}.{img_ext}')
+
+        # Extract card information and store into CARD_INFORMATION_PATH file
+        # ---------------------------------------------------------------------------------------------------------------
+
+        # List to append card information
+        row_to_csv = [current_id]
+
+        for feature in XpathCardFeatures:
+            logger.info(f'[ACTION] Extracting the feature {feature.name}')
+            if feature.name == 'MANA_COST_SYMBOL':
+                try:
+                    row_to_csv.append(get_mana_cost(driver, feature.value))
+                except ManaCostSymbolException:
+                    logger.info(f'[Exception] Problem extracting mana cost symbols for card {current_id}')
+                    row_to_csv.append('')
+            elif feature.name == 'CARD_TEXT':
+                try:
+                    row_to_csv.append(get_card_text(driver, feature.value))
+                except NoCardTextAvailableException:
+                    logger.info(f'[Exception] No card text available for card {current_id}')
+                    row_to_csv.append('')
+            else:
+                try:
+                    row_to_csv.append(get_xpath_text(driver, feature.value))
+                except NoFeatureAvailableException:
+                    logger.info(f'[Exception] No feature {feature.name} available for card {current_id}')
+                    row_to_csv.append('')
+
+        logger.info(f'[STATUS] Card features extraction successful!')
+        logger.info(f'[WRITE] Append card features from card_id: {current_id} into card_information.csv')
+
+        # Append the row_to_csv into the csv file with the extracted card information
+        with open(CARD_INFORMATION_PATH, 'a') as csvFile:
+            writer = csv.writer(csvFile)
+            writer.writerow(row_to_csv)
+        csvFile.close()
+
+        # Quit connection (shut down the geckodriver not only close the tab like driver.close() does)
+        # ---------------------------------------------------------------------------------------------------------------
+        driver.quit()
